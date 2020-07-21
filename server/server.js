5d4307bc8d4c7827f4f974521ccceeea3227fc03
Michael <wang.3101@osu.edu>
2020-07-20 21:31:52 -0700

    Install concurrently dependency and set up dev script

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = expresss();
+const app = express();
 const PORT = 3000;
 
 app.use(express.json());
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   return res
     .status(200)
-    .sendFile(path.resolve(__dirname, './client/index.html'));
+    .sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
 app.listen(PORT, () => {
