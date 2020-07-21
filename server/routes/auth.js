240331b0c007a13c57062d2c2914bcac81918353
Bren Yamaguchi <yamaguchibren@gmail.com>
2020-07-21 14:26:09 -0700

    added callback url to auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,27 @@ const authController = require('../controllers/authController.js');
 
 router.get(
   '/spotify',
-  passport.authenticate('github', {
+  passport.authenticate('spotify', {
     scope: ['user:email'],
   })
+);
+
+router.get('/fail', (req, res) => {
+  res.status(401).send('FAILURE TO AUTHENTICATE');
+});
+
+router.get(
+  '/spotify/callback',
+  passport.authenticate('spotify', {
+    //if failure to authenticate:
+    //placeholder
+    failureRedirect: '/fail',
+  }),
+  authController.saveAccessToken,
+  (req, res) => {
+    //if successful authentication:
+
+    console.log('SUCCESSFUL AUTHENTICATION');
+    res.redirect('/dashboard');
+  }
 );
