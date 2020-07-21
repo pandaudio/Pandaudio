e81d95a267e8716c5adb50b23dc8716c499d32c2
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-07-21 15:38:51 -0700

    add 404 route

--- client/App/index.jsx
+++ client/App/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from '../components/LoginPage';
-import { PrivateRoute } from './PrivateRoute';
+// import { PrivateRoute } from './PrivateRoute';
 
 const App = () => (
   <div>
     <Switch>
       <Route exact path="/" component={LoginPage} />
+      <Route path="*" component={() => '404 NOT FOUND'} />
     </Switch>
   </div>
 );
