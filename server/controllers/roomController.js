fe371453caac170d67fff3e8c6caa3a384ffe04f
Michael <wang.3101@osu.edu>
2020-07-21 11:24:21 -0700

    Finish makeactive middleware in room controller

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -4,8 +4,43 @@ const db = require('../models/roomModels');
  */
 const roomController = {};
 
-roomController.getAllActive = (req, res, next) => {};
-roomController.makeActive = (req, res, next) => {};
+let query = '';
+/**
+ * Get all rooms (from the rooms table) that are active (True)
+ */
+roomController.getAllActive = async (req, res, next) => {
+  try {
+    query = 'SELECT * FROM rooms WHERE rooms.active = True;';
+    const result = await db.query(query);
+    res.locals.activeRooms = result.rows[0];
+    return next();
+  } catch ({ message }) {
+    return next({
+      log: 'Error in room.getAllActive',
+      status: 500,
+      message,
+    });
+  }
+};
+
+/**
+ * Set the room (UUID - in the rooms table) to active (True)
+ */
+roomController.makeActive = async (req, res, next) => {
+  try {
+    const roomId = req.body.uuid;
+    query = 'UPDATE rooms SET active = True WHERE uuid = $1';
+    const values = [roomId];
+    await db.query(query, values);
+    return next();
+  } catch ({ message }) {
+    return next({
+      log: 'Error in room.makeActive',
+      status: 500,
+      message,
+    });
+  }
+};
 roomController.makeInactive = (req, res, next) => {};
 roomController.createRoom = (req, res, next) => {};
 
