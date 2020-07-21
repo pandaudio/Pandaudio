313da11102d8e7ec52ead9cba2d8aad28281eaf1
Michael <wang.3101@osu.edu>
2020-07-21 10:57:43 -0700

    Create boilerplate for room middleware

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,5 +1,12 @@
-const db = require('./../models/roomModels');
-
+const db = require('../models/roomModels');
+/**
+ * Controller for interactions with room tables
+ */
 const roomController = {};
+
+roomController.getAllActive = (req, res, next) => {};
+roomController.makeActive = (req, res, next) => {};
+roomController.makeInactive = (req, res, next) => {};
+roomController.createRoom = (req, res, next) => {};
 
 module.exports = roomController;
