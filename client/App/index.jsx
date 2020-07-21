0627216520122c061e43eb3868c0e9a6078c6d0d
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-07-21 15:21:33 -0700

    add login page

--- client/App/index.jsx
+++ client/App/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-
-// Redux
 import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import LoginPage from '../components/LoginPage';
+// Redux
 import store from '../store';
+import { PrivateRoute } from './PrivateRoute';
 
 const App = () => (
   <div>
-    <div>Hello World</div>
-    <Provider store={store}>
-      <div>Hello World</div>
-    </Provider>
+    <Switch>
+      <Route exact path="/" component={LoginPage} />
+    </Switch>
   </div>
-
 );
+
+// const App = () => (
+//   <div>
+//     <LoginPage />
+//     <div>Hello World</div>
+//     <Provider store={store}>
+//       <div>Hello World</div>
+//     </Provider>
+//   </div>
+// );
 
 export default App;
--- client/components/Login/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Login = () => (
-  <div>
-    <button>Login</button>
-  </div>
-);
