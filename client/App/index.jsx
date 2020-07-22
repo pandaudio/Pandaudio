425fa16f19ff56c419373ce7db34a8b4b350cccf
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-07-21 23:27:30 -0700

    remove chat and dashboard component

--- client/App/index.jsx
+++ client/App/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from '../components/LoginPage';
 import RoomPage from '../components/RoomPage';
+import DashboardPage from '../components/DashboardPage';
 // import { PrivateRoute } from './PrivateRoute';
 import Chat from '../components/Chat.jsx';
 import TestChat from '../components/TestChat.jsx';
@@ -10,14 +11,10 @@ const App = () => (
   <div>
     <Switch>
       <Route exact path="/" component={LoginPage} />
-      <Route exact path="/dashboard" component={RoomPage} />
+      <Route exact path="/dashboard" component={DashboardPage} />
       <Route path="*" component={() => '404 NOT FOUND'} />
     </Switch>
-    <Chat />
   </div>
 );
 
-// Dashboard
-// Create Room
-// Room
 export default App;
--- client/components/DashboardPage/index.jsx
+++ client/components/DashboardPage/index.jsx
@@ -1,27 +1,28 @@
-import React, { useState } from 'react'
-import CreateNewRoomModal from '../CreateNewRoomModal'
+import React, { useState } from 'react';
+import CreateNewRoomModal from '../CreateNewRoomModal';
 
 /**
  * Component: RoomPage
  * Component: CreateRoom Modal
- * render: button create room name 
+ * render: button create room name
  */
 const DashboardPage = () => {
-
   // state hook for a new room name
   const [newRoomName, setNewRoomName] = useState({
     roomName: '',
-  })
+  });
 
-  const [showCreateNewRoomModal, setShowCreateNewRoomModal] = useState(false)
+  const [showCreateNewRoomModal, setShowCreateNewRoomModal] = useState(false);
 
   return (
     <div>
       <h1>This is a DashboardPage</h1>
       <button onClick={() => setShowCreateNewRoomModal(true)}>Create Room</button>
-      {showCreateNewRoomModal && (<CreateNewRoomModal {...{ showCreateNewRoomModal, setShowCreateNewRoomModal }} />)}
+      {showCreateNewRoomModal && (
+        <CreateNewRoomModal {...{ showCreateNewRoomModal, setShowCreateNewRoomModal }} />
+      )}
     </div>
-  )
+  );
 };
 
 export default DashboardPage;
