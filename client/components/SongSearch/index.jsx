6c71f0cb597f19b583a8a72d75f2c6c750e2270c
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-07-21 21:59:56 -0700

    add hooks to SongSearch

--- client/components/SongSearch/index.jsx
+++ client/components/SongSearch/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const SongSearch = () => (
-  <div>
-    <p>Input Text</p>
-    <p>Search Button</p>
-  </div>
-);
+const SongSearch = () => {
+  const [songName, setSongName] = useState('');
+  const [songResults, setSongResults] = useState([]);
+
+  function handleChange(e) {
+    setSongName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // Functionality to search for song
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="searchbar"
+          defaultValue="Enter Song"
+          value={songName}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+};
