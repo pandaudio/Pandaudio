85bbbd5c19f22f4bcb276883575fa0ca61c87232
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-07-21 23:30:50 -0700

    apply formatting rules

--- client/components/CreateNewRoomModal/index.jsx
+++ client/components/CreateNewRoomModal/index.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import axios from 'axios';
 
-const CreateNewRoomModal = (props) => {
-
+const CreateNewRoomModal = props => {
   console.log('In CreateNewROomModal;', props);
   const handleClick = () => {
-    const roomName = document.getElementById("create-room").value;
-    document.getElementById("create-room").value = "";
+    const roomName = document.getElementById('create-room').value;
+    document.getElementById('create-room').value = '';
 
-    axios.post('/api/v1/rooms', {    // ****** check endpoint
-      roomName: roomName,
-      // host: // *********************
-    })
+    axios
+      .post('/api/v1/rooms', {
+        // ****** check endpoint
+        roomName,
+        // host: // *********************
+      })
       .then(function (response) {
         console.log(response);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
-
+  };
 
   return (
     <div>
       <section className="modal-main">
         <h1>This is Create New Room Modal</h1>
-        {/* Have input field, have a button that will grab that input field value*/}
+        {/* Have input field, have a button that will grab that input field value */}
         <input type="text" id="create-room" />
-        <button onClick={handleClick} >Create</button>
+        <button onClick={handleClick}>Create</button>
         <button onClick={() => props.setShowCreateNewRoomModal(false)}>Close</button>
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default CreateNewRoomModal;
