8c97d19e5b047d6be3a1130d5721a36580a979fb
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-07-22 02:45:08 -0700

    add logic to determine host under RoomPage

--- client/components/RoomOption/index.jsx
+++ client/components/RoomOption/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const RoomOption = props => {
+  const history = useHistory();
   const { room } = props;
+
+  function handleClick(e) {
+    e.preventDefault();
+    history.push({
+      pathname: '/room',
+      state: { isHost: false, roomInfo: room },
+    });
+  }
+
   return (
     <div>
       <p>{room.room_name}</p>
-      <button type="submit">Join</button>
+      <button type="submit" onClick={handleClick}>
+        Join
+      </button>
     </div>
   );
 };
--- client/components/RoomPage/index.jsx
+++ client/components/RoomPage/index.jsx
@@ -3,12 +3,20 @@ import PlaybackControls from '../PlaybackControls';
 import SongSearch from '../SongSearch';
 
 const RoomPage = props => {
+  const { location } = props;
   return (
     <div>
-      <SongSearch />
-      <p>Room Name</p>
-      <p>Host Name</p>
-      <PlaybackControls />
+      {location.state.isHost ? <SongSearch /> : null}
+      {location.state.roomInfo.id}
+      <br />
+      {location.state.roomInfo.room_name}
+      <br />
+      {location.state.roomInfo.host}
+      <br />
+      {location.state.roomInfo.active ? 'active' : 'inactive'}
+      <br />
+      {location.state.roomInfo.created_at}
+      {location.state.isHost ? <PlaybackControls /> : null}
     </div>
   );
 };
