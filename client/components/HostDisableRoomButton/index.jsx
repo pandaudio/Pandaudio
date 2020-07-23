688ebe5a3465ef57c650a77dbc41cbab5ad472a9
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-07-23 13:02:46 -0700

    add logic to disable room

--- client/components/HostDisableRoomButton/index.jsx
+++ client/components/HostDisableRoomButton/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 const HostDisableRoomButton = props => {
   const history = useHistory();
@@ -8,8 +9,21 @@ const HostDisableRoomButton = props => {
     e.preventDefault();
     // Disable Room
     // Go Back to DashBoard
-
-    history.goBack();
+    const data = Cookies.get('uuid');
+    fetch('/api/v1/rooms', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(response => {
+        history.goBack();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
 
   return (
