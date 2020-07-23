d714367f7e47197e43abd89bff01f68ee5899c46
Bren Yamaguchi <yamaguchibren@gmail.com>
2020-07-22 18:57:18 -0700

    added player to mount within app component to window, updated gitifnore

--- client/App/index.jsx
+++ client/App/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from '../components/LoginPage';
 import RoomPage from '../components/RoomPage';
@@ -7,15 +7,50 @@ import DashboardPage from '../components/DashboardPage';
 import Chat from '../components/Chat.jsx';
 import TestChat from '../components/TestChat.jsx';
 
-const App = () => (
-  <div>
-    <Switch>
-      <Route exact path="/" component={LoginPage} />
-      <Route exact path="/dashboard" component={DashboardPage} />
-      <Route exact path="/room" component={RoomPage} />
-      <Route path="*" component={() => '404 NOT FOUND'} />
-    </Switch>
-  </div>
-);
+import { PLAYER_INITIALIZE } from '../store/action_types/player';
+import { useDispatch } from 'react-redux';
+
+const App = () => {
+  // NOTE: This is for storing the player in the Store in case window strategy doesn't work
+  // const dispatch = useDispatch();
+  const [token, setToken] = useState(null);
+
+  let playerCheckInterval = null;
+
+  useEffect(() => {
+    playerCheckInterval = setInterval(() => {
+      checkForPlayer();
+    }, 1000);
+  }, []);
+
+  const checkForPlayer = () => {
+    if (window.Spotify !== null) {
+      clearInterval(playerCheckInterval);
+      const newPlayer = new window.Spotify.Player({
+        name: 'Music Zoom Player',
+        getOAuthToken: cb => {
+          cb(token);
+        },
+      });
+
+      // NOTE: This is for storing the player in the Store in case window strategy doesn't work
+      // dispatch({ type: PLAYER_INITIALIZE, payload: newPlayer });
+
+      // store player reference in the window
+      window.globalPlayer = newPlayer;
+    }
+  };
+
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/dashboard" component={DashboardPage} />
+        <Route exact path="/room" component={RoomPage} />
+        <Route path="*" component={() => '404 NOT FOUND'} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
---
+++ client/store/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import playerReducer from './player'
+
+export default combineReducers({
+  playerReducer
+});
